dde920711b1eaec2964a7c39a43f3cf25e04b461
aboulore <aboulore@gmail.com>
2025-01-17 23:50:26 +0100

    feat: strcpy, need to do regular rust test (read rust book)

--- src/ft_strcpy.s
+++ src/ft_strcpy.s
@@ -3,3 +3,16 @@
 
 ft_strcpy:
 		xor rax, rax
+
+.loop:
+		cmp byte [rsi + rax], 0
+		je .out
+		mov bl, byte [rsi + rax]
+		mov byte [rdi + rax], bl
+		inc rax
+		jmp .loop
+
+.out:
+		mov byte [rdi + rax], 0
+		mov rax, rdi
+		ret 
--- test_libasm/src/main.rs
+++ test_libasm/src/main.rs
@@ -1,63 +1,52 @@
-use std::os::raw::c_int; // 32 bits
-use std::os::raw::c_ulonglong; // for size_t
-use std::str::from_utf8_unchecked; // convert bytes in string slice
+use std::ffi::CStr;
+use std::os::raw::c_char;
+use std::ptr;
 
 extern "C" {
-
-   fn ft_strlen(s: *const u8) -> c_ulonglong;
-   fn strlen(s: *const u8) -> c_ulonglong;
-
-    fn ft_strcmp(s1: *const u8, s2: *const u8) -> c_int;
-    fn strcmp(s1: *const u8, s2: *const u8) -> c_int;
+    fn ft_strcpy(dest: *mut c_char, src: *const c_char) -> *mut c_char;
 }
 
-fn main() {
+fn rust_strcpy(dest: &mut [u8], src: &str) -> String {
+    // Ensure the source is null-terminated
+    let src_c = std::ffi::CString::new(src).expect("Failed to create CString");
+    let dest_ptr = dest.as_mut_ptr() as *mut c_char;
 
-    {
-        // STRLEN
-        println!("STRLEN\n");
-
-        let test_str = b"Hello, world!\0";
-
-        unsafe {
-            println!("Test n°1: \"{}\"", from_utf8_unchecked(test_str));
-
-            let length: c_ulonglong = strlen(test_str.as_ptr());
-            println!("With strlen: Length of the string: {}", length);
-            let length: c_ulonglong = ft_strlen(test_str.as_ptr());
-            println!("With ft_strlen.s: Length of the string: {}", length);
-        }
+    unsafe {
+        ft_strcpy(dest_ptr, src_c.as_ptr());
+        let result = CStr::from_ptr(dest_ptr).to_string_lossy().to_string();
+        result
     }
-
-    {
-        // STRCMP
-        println!("\nSTRCMP\n");
-
-        unsafe {
-
-            let s1 = b"zouzou\0";
-            let s2 = b"aouzou\0";
-
-            println!("Test n°1: \"{}\" and \"{}\"", from_utf8_unchecked(s1), from_utf8_unchecked(s2));
-
-            let res: c_int = strcmp(s1.as_ptr(), s2.as_ptr());
-            println!("With strcmp: Res: {}", res);
-            let res: c_int = ft_strcmp(s1.as_ptr(), s2.as_ptr());
-            println!("With ft_strcmp: Res: {}", res);
-        }
-
-        unsafe {
-
-            let s1 = b"\0";
-            let s2 = b"\0";
-
-            println!("Test n°2: \"{}\" and \"{}\"", from_utf8_unchecked(s1), from_utf8_unchecked(s2));
-
-            let res: c_int = strcmp(s1.as_ptr(), s2.as_ptr());
-            println!("With strcmp: Res: {}", res);
-            let res: c_int = ft_strcmp(s1.as_ptr(), s2.as_ptr());
-            println!("With ft_strcmp: Res: {}", res);
-        }
-    }
-
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_strcpy_basic() {
+        let mut dest = [0u8; 100];
+        let src = "Hello, world!";
+        let result = rust_strcpy(&mut dest, src);
+
+        assert_eq!(result, src);
+    }
+
+    #[test]
+    fn test_strcpy_empty() {
+        let mut dest = [0u8; 100];
+        let src = "";
+        let result = rust_strcpy(&mut dest, src);
+
+        assert_eq!(result, src);
+    }
+
+    #[test]
+    fn test_strcpy_with_special_chars() {
+        let mut dest = [0u8; 100];
+        let src = "Hello\nWorld\t!";
+        let result = rust_strcpy(&mut dest, src);
+
+        assert_eq!(result, src);
+    }
+}
+
